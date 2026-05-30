23f2d8ff059aa44770b83c863196bdc33fd764f8
mengronal <mengronal@gmail.com>
2026-05-30 11:16:00 +0700

    upate product

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -36,6 +36,14 @@
                                 <span class="hide-menu">Dashboard</span>
                             </a>
                         </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-layout-dashboard"></i>
+                                </span>
+                                <span class="hide-menu">Product</span>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
