1947ce0a55dcb01f10044f78572680f92a0d8477
mengronal <mengronal@gmail.com>
2026-05-30 10:47:56 +0700

    update view

---
+++ resources/views/pos/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.pos')
+
+@section('content')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    try {
-        DB::connection()->getPdo();
-        return "Connected successfully to: " . DB::connection()->getDatabaseName();
-    } catch (\Exception $e) {
-        return "Could not connect to the database. Error: " . $e->getMessage();
-    }
+Route::get('/',function (){
+    return view('pos.dashboard');
+});
+Route::get('/admin/dashboard',function (){
+    return view('admin.Admin_dashboard');
 });
