edd1135773205ec17ad515db2a79eb6a18ad54ed
VibolMeanLeap <meanleap229@gmail.com>
2026-06-20 10:12:46 +0700

    upate admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -29,46 +29,7 @@
   
 </head>
 
-<<<<<<< HEAD
-<body>
-    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed">
-        <aside class="left-sidebar">
-            <div>
-                <div class="brand-logo d-flex align-items-center justify-content-between">
-                    <a href="./index.html" class="text-nowrap logo-img">
-                        <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
-                    </a>
-                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-                        <i class="ti ti-x fs-8"></i>
-                    </div>
-                </div>
-                <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-                    <ul id="sidebarnav">
-                        <li class="nav-small-cap">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Home</span>
-                        </li>
-                        
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="./index.html" aria-expanded="false">
-                                <span>
-                                    <i class="ti ti-layout-dashboard"></i>
-                                </span>
-                                <span class="hide-menu">Dashboard</span>
-                            </a>
-                        </li>
-                          <li class="sidebar-item">
-                            <a class="sidebar-link" href="./index.html" aria-expanded="false">
-                                <span>
-                                    <i class="ti ti-layout-dashboard"></i>
-                                </span>
-                                <span class="hide-menu">Category</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-=======
+
 <body class="bg-theme bg-theme1">
  
 <!-- Start wrapper-->
@@ -135,7 +96,7 @@
       
       <li>
         <a href="">
-          <i class="zmdi zmdi-face"></i> <span>Reports</span>
+          <i class="fa-solid fa-chart-simple"></i> <span>Reports</span>
         </a>
       </li>
       <li>
