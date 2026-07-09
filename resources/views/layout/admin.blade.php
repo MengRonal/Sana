84b759a8f6a55ff23b032d290259558c8e337e5a
mengronal <mengronal@gmail.com>
2026-07-09 22:58:06 +0700

    again admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -250,7 +250,6 @@
             </div>
         </div>
         @yield('content')
-<body class="bg-theme bg-theme1">
  
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
